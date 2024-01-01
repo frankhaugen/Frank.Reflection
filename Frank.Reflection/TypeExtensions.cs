92276d5dd88b44a3df6ec02b72433601c417331c
Frank R. Haugen <frank.haugen@gmail.com>
2024-01-01 04:52:41 +0100

    Extend TypeExtensions.cs with additional display methods
    The type extension methods in TypeExtensions.cs have been extended to include additional methods like GetFullDisplayName, GetFriendlyName, and GetFullFriendlyName. These additions aim to provide more descriptive information for a specified Type, including its namespace and its friendly name formats. The code has also been refactored for better readability and efficiency.

--- Frank.Reflection/TypeExtensions.cs
+++ Frank.Reflection/TypeExtensions.cs
@@ -1,13 +1,4 @@
-﻿// This code is copied from Namotion.Reflection and modified:
-//-----------------------------------------------------------------------
-// <copyright file="ReflectionExtensions.cs" company="NSwag">
-//     Copyright (c) Rico Suter. All rights reserved.
-// </copyright>
-// <license>https://github.com/NSwag/NSwag/blob/master/LICENSE.md</license>
-// <author>Rico Suter, mail@rsuter.com</author>
-//-----------------------------------------------------------------------
-
-namespace Frank.Reflection;
+﻿namespace Frank.Reflection;
 
 /// <summary>Provides extension methods for reflection.</summary>
 public static class TypeExtensions
@@ -15,35 +6,73 @@ public static class TypeExtensions
     /// <summary>Gets a human readable type name (e.g. DictionaryOfStringAndObject).</summary>
     /// <param name="type">The type.</param>
     /// <returns>The type name.</returns>
-    public static string GetDisplayName(this Type type)
+    public static string GetDisplayName(this Type type) => GetDisplayNameInternal(type);
+
+    /// <summary>
+    /// Gets the full display name of a Type. e.g. System.Collections.Generic.DictionaryOfStringAndObject
+    /// </summary>
+    /// <param name="type">The Type for which to retrieve the full display name.</param>
+    /// <returns>The full display name of the Type, which includes the namespace and the display name.</returns>
+    public static string GetFullDisplayName(this Type type)
+    {
+        var displayName = GetDisplayNameInternal(type);
+        var namespaceName = type.Namespace;
+        return namespaceName == null ? displayName : namespaceName + "." + displayName;
+    }
+
+    /// <summary>
+    /// Gets the friendly name of the specified Type. e.g. Dictionary&lt;string, object&gt;
+    /// </summary>
+    /// <param name="type">The Type to get the friendly name for.</param>
+    /// <returns>The friendly name of the specified Type.</returns>
+    public static string GetFriendlyName(this Type type) => GetDisplayNameInternal(type);
+
+    /// <summary>
+    /// Returns the full friendly name of the specified <see cref="Type"/>. e.g. System.Collections.Generic.Dictionary&lt;System.String, System.Object&gt;
+    /// </summary>
+    /// <param name="type">The <see cref="Type"/> to get the full friendly name for.</param>
+    /// <returns>The full friendly name of the specified <see cref="Type"/>.</returns>
+    public static string GetFullFriendlyName(this Type type)
+    {
+        var friendlyName = GetFriendlyNameInternal(type);
+        var namespaceName = type.Namespace;
+        return namespaceName == null ? friendlyName : namespaceName + "." + friendlyName;
+    }
+    
+    private static string GetFriendlyNameInternal(this Type type)
     {
         var nType = type;
         if (nType.IsConstructedGenericType)
-        {
+            return GetName(nType).FirstToken('`') + "<" +
+                   string.Join(", ", nType.GenericTypeArguments
+                       .Select(GetFriendlyName)) + ">";
+
+        return GetName(nType);
+    }
+    
+    private static string GetDisplayNameInternal(this Type type)
+    {
+        var nType = type;
+        if (nType.IsConstructedGenericType)
             return GetName(nType).FirstToken('`') + "Of" +
                    string.Join("And", nType.GenericTypeArguments
-                                           .Select(GetDisplayName));
-        }
+                       .Select(GetDisplayName));
 
         return GetName(nType);
     }
 
-    private static string GetName(Type type)
-    {
-        return
-            type.Name == "Int16" ? GetNullableDisplayName(type, "Short") :
-            type.Name == "Int32" ? GetNullableDisplayName(type, "Integer") :
-            type.Name == "Int64" ? GetNullableDisplayName(type, "Long") :
-            GetNullableDisplayName(type, type.Name);
-    }
+    private static string GetName(Type type) =>
+        type.Name switch
+        {
+            "Int16" => GetNullableDisplayName(type, "Short"),
+            "Int32" => GetNullableDisplayName(type, "Integer"),
+            "Int64" => GetNullableDisplayName(type, "Long"),
+            _ => GetNullableDisplayName(type, type.Name)
+        };
 
-    private static string GetNullableDisplayName(Type type, string actual)
-    {
-        return (IsNullableType(type) ? "Nullable" : "") + actual;
-    }
+    private static string GetNullableDisplayName(Type type, string actual) 
+        => (IsNullableType(type) ? "Nullable" : "") + actual;
 
-    private static bool IsNullableType(Type type)
-    {
-        return type.IsConstructedGenericType && type.GetGenericTypeDefinition() == typeof(Nullable<>);
-    }
+    private static bool IsNullableType(Type type) 
+        => type.IsConstructedGenericType && type.GetGenericTypeDefinition() == typeof(Nullable<>);
 }
